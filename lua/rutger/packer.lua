64cc01960682bd2cf186b90a3e81a7bea789857b
rutger beltman <rutbelt@gmail.com>
2023-03-26 01:59:08 +0100

    formatting

--- lua/rutger/packer.lua
+++ lua/rutger/packer.lua
@@ -4,64 +4,68 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-          vim.cmd('colorscheme rose-pine')
-      end
-  })
+   -- Packer can manage itself
+   use 'wbthomason/packer.nvim'
 
    use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      -- or                            , branch = '0.1.x',
+      requires = { { 'nvim-lua/plenary.nvim' } }
+   }
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+   use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+         vim.cmd('colorscheme rose-pine')
+      end
+   })
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+   use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+         -- LSP Support
+         { 'neovim/nvim-lspconfig' },
+         { 'williamboman/mason.nvim' },
+         { 'williamboman/mason-lspconfig.nvim' },
 
-  -- A pretty list for showing diagnostics, references, telescope results,
-  -- quickfix and location lists to help you solve all the trouble your code
-  -- is causing
-  use({
+         -- Autocompletion
+         { 'hrsh7th/nvim-cmp' },
+         { 'hrsh7th/cmp-buffer' },
+         { 'hrsh7th/cmp-path' },
+         { 'saadparwaiz1/cmp_luasnip' },
+         { 'hrsh7th/cmp-nvim-lsp' },
+         { 'hrsh7th/cmp-nvim-lua' },
+
+         -- Snippets
+         { 'L3MON4D3/LuaSnip' },
+         { 'rafamadriz/friendly-snippets' },
+      }
+   }
+
+   -- A pretty list for showing diagnostics, references, telescope results,
+   -- quickfix and location lists to help you solve all the trouble your code
+   -- is causing
+   use({
       "folke/trouble.nvim",
       config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
+         require("trouble").setup {
+            icons = false,
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+         }
       end
-  })
+   })
 
-  use("nvim-treesitter/nvim-treesitter",{ run = ":TSUpdate"})
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+   use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+   use("mbbill/undotree")
+   use("tpope/vim-fugitive")
+   use 'neovim/nvim-lspconfig'
+   use 'simrat39/rust-tools.nvim'
 
+   -- Debugging
+   use 'nvim-lua/plenary.nvim'
+   use 'mfussenegger/nvim-dap'
 end)
-
--- lua/rutger/remap.lua
+++ lua/rutger/remap.lua
@@ -22,11 +22,11 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- interact with clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Remove without filling buffer
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -44,4 +44,3 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 -- replace current
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
