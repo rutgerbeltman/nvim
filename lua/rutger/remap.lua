ba17602bd0972d9c8f09e2774b3f14c2243cec68
rutger beltman <rutbelt@gmail.com>
2023-03-26 01:27:33 +0100

    add terminal escape and m odify tab spacesw

--- lua/rutger/remap.lua
+++ lua/rutger/remap.lua
@@ -28,7 +28,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- Remove without filling buffer
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -38,6 +37,9 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- exit terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- replace current
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/rutger/set.lua
+++ lua/rutger/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 3
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -29,7 +29,7 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "86"
 
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
